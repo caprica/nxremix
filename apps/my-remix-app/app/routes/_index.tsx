8e813bbd7ef9431ff6677d44a83531c27bcd1d4a
Mark Lee <mark.lee@capricasoftware.co.uk>
2024-11-01 10:12:41 +0000

    Use dependency from library project in Remix application
    Successfully import a component from the library project in the
    mono repo into the Remix application.
    
    This is imported and runs correctly.

--- apps/my-remix-app/app/routes/_index.tsx
+++ apps/my-remix-app/app/routes/_index.tsx
@@ -1,8 +1,10 @@
+import { MyLib } from '@nxremix/libs/my-lib';
 import NxWelcome from '../nx-welcome';
 
 export default function Index() {
   return (
     <div>
+      <MyLib />
       <NxWelcome title={'my-remix-app'} />
     </div>
   );
