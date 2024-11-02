8ea709de2e0481a0be4036051b5597002a74f374
Mark Lee <mark.lee@capricasoftware.co.uk>
2024-11-02 11:35:25 +0000

    Fix indentation properly this time

--- apps/my-remix-app/vite.config.ts
+++ apps/my-remix-app/vite.config.ts
@@ -3,29 +3,29 @@ import { defineConfig } from 'vite';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 declare module '@remix-run/node' {
-    interface Future {
-      v3_singleFetch: true;
-    }
+  interface Future {
+    v3_singleFetch: true;
   }
+}
 
-  export default defineConfig({
-    root: __dirname,
-    plugins: [
-      remix({
-        future: {
-          v3_fetcherPersist: true,
-          v3_relativeSplatPath: true,
-          v3_throwAbortReason: true,
-          v3_singleFetch: true,
-          v3_lazyRouteDiscovery: true,
-        },
-      }),
-      nxViteTsPaths(),
-    ],
-    server: {
-      port: 3000,
-      fs: {
-        allow: ['..']
+export default defineConfig({
+  root: __dirname,
+  plugins: [
+    remix({
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+        v3_singleFetch: true,
+        v3_lazyRouteDiscovery: true,
       },
+    }),
+    nxViteTsPaths(),
+  ],
+  server: {
+    port: 3000,
+    fs: {
+      allow: ['..']
     },
-  });
+  },
+});
